f9db368452b6d46008d72eda11389054d3b68099
Anthony Loffredo <loffredo.ap@gmail.com>
2023-02-10 13:28:26 -0700

    Autocommit
    Autocommit

--- Interfaces/IPersistableEmailMessage.cs
+++ Interfaces/IPersistableEmailMessage.cs
@@ -5,7 +5,9 @@ namespace Penguin.Cms.Email.Abstractions.Interfaces
     public interface IPersistableEmailMessage : IEmailMessage
     {
         new string BCCRecipients { get; set; }
+
         new string CCRecipients { get; set; }
+
         new string Recipients { get; set; }
     }
 }
